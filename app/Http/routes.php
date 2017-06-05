761cf0575699ec7276a862f5e0366a014b511d0d
Alerbethe <2458360373@qq.com>
2017-06-05 03:16:49 +0000

    finish static pages

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,4 +1,7 @@
 <?php
+Route::get('/',  'StaticPagesController@home');
+Route::get('/help', 'StaticPagesController@help');
+Route::get('/about', 'StaticPagesController@about');
 
 /*
 |--------------------------------------------------------------------------
@@ -11,6 +14,4 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
